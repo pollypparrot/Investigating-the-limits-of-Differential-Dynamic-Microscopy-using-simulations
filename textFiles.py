d86f3edb6d234a919c39fa54184c3a13f5337e21
Holly Chandler <76847686+pollypparrot@users.noreply.github.com>
2023-10-05 17:24:37 +0100

    Read and write tab delimited files

--- textFiles.py
+++ textFiles.py
@@ -4,10 +4,26 @@
 #Text Files sorter
 #27/09/23
 
+import split
   
 #create new file
 #WARNING will overwrite data in file if it already exists (useful for running program many times but be careful)
-def createTabDelimitedFile (fileName, data):
+def createFile (fileName):
+    file = open(fileName, "x")
+    file.close()
+
+def createTabDelimitedFile(fileName, data):
     file = open(fileName, "w")
+    for item in data:
+        file.write(str(data[item])+"\t")
+    file.close()
+
+def readTabDelimitedFile(fileName):
+    file = open(fileName,"r")
+    txt = file.read()
+    print(txt)
     file.close()
     
+    
+createTabDelimitedFile("Xcoord",[2,4,5,6,4,2,4,5,3])
+readTabDelimitedFile("Xcoord")
